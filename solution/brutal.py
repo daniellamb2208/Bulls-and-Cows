e79912391752b33bd594a4e574a5067c514824ba
daniellamn2208 <firstsuccess1106@yahoo.com.tw>
2021-11-01 03:29:34 +0800

    Take arguments by input

--- solution/brutal.py
+++ solution/brutal.py
@@ -1,18 +1,21 @@
 import random #choice
 from itertools import permutations
 
+digit = 6
+t = 10  # loop times
+
 def answer():
     x = [0,1,2,3,4,5,6,7,8,9]
     random.shuffle(x)
 
-    return x[0:4]
+    return x[0:digit]
 
 def diff(a, b):
 
     A, B = 0, 0
 
-    for i in range(4):
-        for j in range(4):
+    for i in range(digit):
+        for j in range(digit):
             if a[i] == b[j] and i == j:
                 A = A + 1
             elif a[i] == b[j]:
@@ -20,7 +23,10 @@ def diff(a, b):
     return A, B
 
 def strize(z):
-    return str(z[0])+str(z[1])+str(z[2])+str(z[3])
+    s = ""
+    for i in range(digit):
+        s += str(z[i])
+    return s
 
 def narrow(z, g, A, B):
     # ref:https://neilchennc.blogspot.com/2010/05/ab.html
@@ -35,12 +41,13 @@ def narrow(z, g, A, B):
 def guess(cc):
     
     x = answer()
+    y = [0,1,2,3,4,5,6,7,8,9] # Elements
+    z = list(permutations(y, digit))  # Universal 
+    
     print("---------------------------")
+    print("Total possibilities is {}".format(len(z)))
     print("{}: Answer is {}".format(cc, strize(x)))
 
-    y = [0,1,2,3,4,5,6,7,8,9] # Elements
-    z = list(permutations(y, 4))  # Universal 
-    
     c = 1
     while True:
         _guess = random.choice(z)
@@ -48,21 +55,28 @@ def guess(cc):
         (A, B) = diff(x, _guess)
         print("{}: Guess {} is {}A{}B.".format(c, strize(_guess), A, B))
         
-        if (A, B) == (4, 0):
+        if (A, B) == (digit, 0):
           print("{}th guesses to find answer".format(c))
           return c
 
         c = c + 1
         z = narrow(z, _guess, A, B)
-        print("Remaining possible answer: {}".format(len(z)))
+        print("Remaining possible answers: {}".format(len(z)))
 
 # plan to multi-threading
 
 if __name__ == "__main__":
 
-    t = 100
-    p = 0
-    for i in range(1,t+1):
-      p = p + guess(i)
-      
-    print("Average guess count is {}".format(p/t))
+    while True:
+        digit = int(input("Please enter which digit to guess:"))
+        t = int(input("Please enter how many round to guess:"))
+        if digit not in range(1,11) and t > 100:
+            pass
+        if digit == 0:
+            print("Bye Bye")
+            break
+        p = 0
+        for i in range(1,t+1):
+            p = p + guess(i)
+        
+        print("Average guess count is {}".format(p/t))
